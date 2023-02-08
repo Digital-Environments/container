64f3aa6c694650daa04a65869c233e8d913aa5db
MembaSovTech <membathisi.mcetywa@sovtech.com>
2023-02-08 09:14:27 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  const who: String = "datBoy";
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Digital Environments</h1>
+      </div>
+      <div className="section">
+        <p>Exploring digital environments.</p>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
