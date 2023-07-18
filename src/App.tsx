a33adc9c5375a8e792bfc0627a952d6eebcb3cc6
Membathisi Mcetywa <iamemba10gmail.com>
2023-07-18 11:37:10 +0200

    feature(App): added jokes call, we always want the jokes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import React, {useEffect} from "react";
+
 import Header from "./components/Header/Header";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 function App() {
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((joke) => {
+      console.log(joke.value);
+    });
+}, [])
 
   return (
     <div className="App">
