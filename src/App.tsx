7b9c278d32ee59ba8e04636a8d8b2fcfb03888b3
Membathisi Mcetywa <iamemba10gmail.com>
2023-07-18 09:59:08 +0200

    feature(App): re did app tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import Header from "./components/Header/Header";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
 function App() {
-  const who: String = "datBoy";
 
   return (
     <div className="App">
-      <div className="header">
-        <h1>
-          Digital <span style={{ color: "#87ceeb" }}>Environments</span>.
-        </h1>
-      </div>
-      <div className="underline"></div>
+        <body>
+          <Header />
+         <VideoPlayer />
+        </body>  
     </div>
   );
 }
