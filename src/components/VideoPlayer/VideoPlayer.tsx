ef70385e77c08dde2364bbeac438a5189cce8ce9
Membathisi Mcetywa <iamemba10gmail.com>
2023-07-19 08:28:37 +0200

    feature(video player): added styled component

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,13 +1,25 @@
 import './videoplayer.css'
 import video from '../../assets/video_asset.mp4'
+import styled from 'styled-components'
+
+const Section = styled.div`
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    height: 100vh;
+    min-width: 100%;
+    min-height: 100%;
+    z-index: -1;
+`
 
 const VideoPlayer = () => {
 
 return (
+<Section>
 <video autoPlay muted loop className='video'>
 <source src={video} type="video/mp4" />
 </video>
+</Section>
 )
 }
-
 export default VideoPlayer;
